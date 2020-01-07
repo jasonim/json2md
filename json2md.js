43cf013620a18cd6b58ece1658908b300f1fd64e
jason <hk081508@gmail.com>
2020-01-07 12:44:51 +0800

    support big file

--- json2md.js
+++ json2md.js
@@ -1,5 +1,6 @@
 var path = require("path"); //系统路径模块
 var fs = require("fs"); //文件模块
+const slugify = require("slugify");
 
 // var file = path.join(__dirname, 'data.json'); //文件路径，__dirname为当前运行js文件的目录
 // const data = fs.readFileSync(file, 'utf-8');
@@ -9,9 +10,15 @@ var fs = require("fs"); //文件模块
 function parseContent(content, num) {
   content.forEach((item, index) => {
     let title = item.title;
+    let urlPath = slugify(title, {
+      replacement: "_",
+      remove: /[*+~.()'"!:@【】]/g, // regex to remove characters
+      lower: true // result in lower case
+    });
+    urlPath = urlPath.replace(/[\u4e00-\u9fa5]/g, "");
     let outputFile = path.join(
       __dirname,
-      "dist/" + title.replace(new RegExp(" ", "gm"), "_") + ".md"
+      "dist/" + urlPath
     );
 
     let HEAD = `
@@ -37,8 +44,8 @@ categories: baidu
       `\n\n[How to download](https://bpcam.bemobtrk.com/go/2ceec3aa-1ca2-46d6-b9ff-aaa5c184517c?jno=${num +
         index})`
     );
-    console.log("count ", num, " index: ", index, " ", num + index);
-    fs.writeFile(outputFile, inputData, () => {});
+    console.log("count ", num, " index: ", index, " ", num + index, urlPath);
+    fs.writeFile(outputFile + `_${num + index}.md`, inputData, () => {});
   });
 
   return num + content.length;
